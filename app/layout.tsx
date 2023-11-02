e69c2f3b91cbdb0d7332c826e28e33e895e3d40e
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2023-11-02 09:40:05 +0600

    Customize Redix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@radix-ui/themes/styles.css'
+import './theme-config.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '@/app/NavBar'
-import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -20,7 +25,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Theme>
+                <Theme accentColor="purple" appearance="light">
                     <NavBar />
                     <main className="p-5">{children}</main>
                 </Theme>
