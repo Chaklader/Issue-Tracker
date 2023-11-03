9ecdeece4ac0e3acb404a7ee282d55e1b89893b9
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2023-11-03 10:14:02 +0600

    Add edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import prisma from '@/prisma/client'
 import { notFound } from 'next/navigation'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import ReactMarkdown from 'react-markdown'
-import delay from 'delay'
+import { Pencil2Icon } from '@radix-ui/react-icons'
+import Link from 'next/link'
 
 interface Props {
     params: { id: string }
@@ -18,16 +19,30 @@ const IssueDetailPage = async ({ params }: Props) => {
     if (!issue) notFound()
 
     return (
-        <div>
-            <Heading as="h1">{issue.title}</Heading>
-            <Flex gap="3" py="2">
-                <IssueStatusBadge status={issue.status} />
-                <Text>{issue.createAt.toDateString()}</Text>
-            </Flex>
-            <Card className="prose" mt="4">
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-        </div>
+        <Grid
+            columns={{
+                initial: '1',
+                md: '2',
+            }}
+            gap="5"
+        >
+            <Box>
+                <Heading as="h1">{issue.title}</Heading>
+                <Flex gap="3" py="2">
+                    <IssueStatusBadge status={issue.status} />
+                    <Text>{issue.createAt.toDateString()}</Text>
+                </Flex>
+                <Card className="prose" mt="4">
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
 
