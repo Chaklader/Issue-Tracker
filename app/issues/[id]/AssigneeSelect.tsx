d2b009439838d1fc0d4405da515593d539dcb070
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2023-11-04 18:20:18 +0600

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -8,33 +8,25 @@ import { Skeleton } from '@/app/components'
 import toast, { Toaster } from 'react-hot-toast'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-    const {
-        data: users,
-        error,
-        isLoading,
-    } = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get<User[]>('/api/users').then((res) => res.data),
-        staleTime: 60 * 1000,
-        retry: 3,
-    })
+    const { data: users, error, isLoading } = UseUsers()
 
     if (isLoading) return <Skeleton />
     if (error) return null
 
+    const assignIssue = async (userId: string) => {
+        try {
+            await axios.patch('/api/issues/' + issue.id, {
+                assignedToUserId: userId || null,
+            })
+        } catch (error) {
+            toast.error("Changes couldn't saved")
+        }
+    }
     return (
         <>
             <Select.Root
                 defaultValue={issue.assignedToUserId || ''}
-                onValueChange={async (userId) => {
-                    try {
-                        await axios.patch('/api/issues/' + issue.id, {
-                            assignedToUserId: userId || null,
-                        })
-                    } catch (error) {
-                        toast.error("Changes couldn't saved")
-                    }
-                }}
+                onValueChange={assignIssue}
             >
                 <Select.Trigger placeholder="Assign..." />
                 <Select.Content>
@@ -53,5 +45,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
         </>
     )
 }
+
+const UseUsers = () =>
+    useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: () => axios.get<User[]>('/api/users').then((res) => res.data),
+        staleTime: 60 * 1000,
+        retry: 3,
+    })
 
 export default AssigneeSelect
